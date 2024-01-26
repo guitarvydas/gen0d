d58c455300e4c43fb7b251fe9c5eba4b78f1086e
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-01-26 14:38:58 -0500

    generates code for init and Rest

--- generate.js
+++ generate.js
@@ -24,10 +24,12 @@ semantics.addOperation('gen', {
     anychar : function (c) { return this.sourceString; },
     
     escapedChar_dquote : function (_) { return '"'; },
-    codeMacro: function (dq1, namecs, _defcode, ws2, cs, dq2) { return `❨${encodeURIComponent (namecs.gen ().join (''))}❩❨{${encodeURIComponent (cs.gen ().join (''))}}❩`; },
+    codeMacro: function (dq1, namecs, _defcode, ws2, cs, dq2) { return `❨${encodeURIComponent (namecs.gen ().join (''))}❩❨${encodeURIComponent (cs.gen ().join (''))}❩`; },
     codechar_nested : function (dq1, cs, dq2) { return "\\\"" + cs.gen ().join ('') + "\\\""; },
     codechar_other : function (c) { return this.sourceString; },
+    codechar_next: function (x) { return " zd.send (eh=eh, port=\"\", datum=zd.new_datum_bang (), causingMessage=msg) "; },
     codeMark: function (x) { return "∷"; },
+    nextMark: function (x) { return "⇒"; },
     _terminal: function () { return this.sourceString; },
     _iter: function (...children) { return children.map(c => c.gen ()); },
 });
