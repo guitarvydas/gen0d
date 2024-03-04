f68cb9dbb25bc000681d2df5f37f5ee495ae8cff
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-03-04 07:53:52 -0500

    rest_of_lines_after_first_newline_or_space working

--- odinsupport.js
+++ odinsupport.js
@@ -25,9 +25,19 @@ _ = {
     },
     rest_of_lines_after_first_newline_or_space : function (s0) {
 	let lines_split_on_newline = s0.split ("&#xa;");
-	let rest_after_newline = lines_split_on_newline.slice (1);
-	let rest_after_newline_string = rest_after_newline.join ("\n");
-	return rest_after_newline_string;
+	let rest_after_newline_string = "";
+	if (1 < lines_split_on_newline.length) {
+	    let rest_after_newline = lines_split_on_newline.slice (1);
+	    rest_after_newline_string = rest_after_newline.join ("\n");
+	    return rest_after_newline_string;
+	} else {
+	    rest_after_newline_string = lines_split_on_newline [0];
+	    let split_on_space = rest_after_newline_string.split (" ");
+	    let rest_after_space = split_on_space.slice (1);
+	    let rest_after_space_as_string = rest_after_space.join (" ");
+	    
+	    return rest_after_space_as_string;
+	}	    
     },
     name_of : function (s) {
 	let name = _.delete_reserved_characters (_.decode0DForName (_.first_line (s)));
